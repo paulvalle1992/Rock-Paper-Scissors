7ea7d54f3273493f7893f752b39890b9c77741b6
Paul V <paulvalle1992@gmail.com>
2023-02-15 15:44:38 -0800

    added the computer selection function for rock paper scissors, wil randomly choose

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const selectionPool = ["rock", "paper", "scissors"];
+  return selectionPool[Math.floor(Math.random() * selectionPool.length)];
+ }
+let computerSelection = getComputerChoice()
+
+ function playRound(playerSelection, computerSelection)
