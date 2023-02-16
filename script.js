28867aa5a51197e629ce63b75adf8c8d454533a5
Paul V <paulvalle1992@gmail.com>
2023-02-15 18:06:02 -0800

    added function playRound to play a round of rock paper scissorss and give appropriate responses if win lose or tie.

--- script.js
+++ script.js
@@ -4,4 +4,30 @@ function getComputerChoice() {
  }
 let computerSelection = getComputerChoice()
 
- function playRound(playerSelection, computerSelection)
+
+let userSubmit = prompt("Play rock, paper, scissors vs a robot! First to 5 wins!");
+   if (userSubmit.toLowerCase() === "rock") {
+   userSelection = "rock";
+ } else if (userSubmit.toLowercase() === "paper") {
+   userSelection = "paper";
+ } else if (userSubmit.toLowerCase() === "scissors") {
+   userSelection = "scissors";
+ }
+
+ let playerSelection = userSelection;
+
+ function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "Rock beats scissors, you win!";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "Paper beats rock, you win!!";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "Scissors beats paper, you win!!";
+    }
+    else if (playerSelection === computerSelection) {
+        return "It's a tie!!! Try again! :)";
+    }
+    else return "You lose! Try again! :(";
+ }
