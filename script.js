141bdc87d599323ba1c3c7c998361a41ae5c0b29
Paul V <paulvalle1992@gmail.com>
2023-03-09 15:52:47 -0800

    used forEach loop to select each button in the nodelist to shorten code, added userScore and computerScore variable set it as number to work on scorekeeper function

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
 document.body.style.backgroundColor = "#6a6a6a";
 const selectionPool = ["rock", "paper", "scissors"];
+let computerSelection = getComputerChoice();
+let playerWins = "You win!";
+let computerWins = "You lose!";
+let noOneWins = "It\'s a tie!";
 
 function getComputerChoice() {
-  return selectionPool[Math.floor(Math.random() * selectionPool.length)];
- };
+   return selectionPool[Math.floor(Math.random() * selectionPool.length)];
+}
+// //rock is 0, paper is 1, scissors is 2
+// function playRound(playerSelection, computerSelection) {
+//     if (playerSelection === selectionPool[0] && computerSelection === selectionPool[2]) {
+//         return `${playerWins} Rock beats scissors!!`;
+//     }
+//     else if (playerSelection === selectionPool[1] && computerSelection === selectionPool[0]) {
+//         return `${playerWins} Paper beats rock!!`;
+//     }
+//     else if (playerSelection === selectionPool[2] && computerSelection === selectionPool[1]) {
+//         return `${playerWins} Scissors beats paper!!`;
+//     }
+//     else if (playerSelection === computerSelection) {
+//         return `${noOneWins} Try again. :)`;
+//     }
+//     else return  `${computerWins} Try again. :(`;
+//   }
+const button = document.querySelectorAll('button');
+button.forEach((button) => {
+    button.addEventListener('click', game);
+});
 
-let computerSelection = getComputerChoice();
+function game(e) { 
+    if (e.target.classList.contains('btn')) {
+        for (i = 0; i < 5; i++) {
+            console.log(i);
+            }
+        }
+}
 
- let playerWins = "You win!";
- let computerWins = "You lose!";
- let noOneWins = "It\'s a tie!";
+let userScore = Number(document.getElementById('userScore').textContent);
+let computerScore = Number(document.getElementById('computerScore').textContent);
 
-//rock is 0, paper is 1, scissors is 2
 
- function playRound(playerSelection, computerSelection) {
-    if (playerSelection === selectionPool[0] && computerSelection === selectionPool[2]) {
-        return `${playerWins} Rock beats scissors!!`;
-    }
-    else if (playerSelection === selectionPool[1] && computerSelection === selectionPool[0]) {
-        return `${playerWins} Paper beats rock!!`;
-    }
-    else if (playerSelection === selectionPool[2] && computerSelection === selectionPool[1]) {
-        return `${playerWins} Scissors beats paper!!`;
-    }
-    else if (playerSelection === computerSelection) {
-        return `${noOneWins} Try again. :)`;
-    }
-    else return  `${computerWins} Try again. :(`;
-  }
 
-//   function game() { 
-//     for (i = 0; i < 5; i++) {
-//         let computerSelection = getComputerChoice();
-//     let playerSelection = prompt("Play rock, paper, scissors vs a robot! First to 5 wins!").toLowerCase();
-//        if (playerSelection === selectionPool[0] || playerSelection === selectionPool[1] || playerSelection === selectionPool[2]) {
-//         console.log(playRound(playerSelection, computerSelection));    
-//        }
-//       else {
-//        return alert("Only works if putting rock, paper or scissors.");
-//       } 
-//      }
-//    }
-
-// game();
