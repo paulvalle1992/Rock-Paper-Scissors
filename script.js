a1a4b417de0c2e7895fb39adc2bc4d4710e5bd02
Paul V <paulvalle1992@gmail.com>
2023-03-09 19:46:12 -0800

    added two lines of code that updates the numbers to reflect computer and user wins

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.body.style.backgroundColor = "#6a6a6a";
 const selectionPool = ["rock", "paper", "scissors"];
 let computerSelection = getComputerChoice();
+let playerSelection = '';
 let playerWins = "You win!";
 let computerWins = "You lose!";
 let noOneWins = "It\'s a tie! Try again young padawan.";
 let userScore = Number(document.getElementById('userScore').textContent);
 let computerScore = Number(document.getElementById('computerScore').textContent);
 const resultsText = document.getElementById('resultsText');
+
 
 function getComputerChoice() {
    return selectionPool[Math.floor(Math.random() * selectionPool.length)];
@@ -20,32 +22,39 @@ function gameRock() {
     if (computerSelection == selectionPool[0]) {
         return resultsText.textContent = noOneWins;
     } else if (computerSelection == selectionPool[1]) {
-        return resultsText.textContent = `${computerWins} Paper covers rock. I do not grant you the rank of Master.`;
-    } else return resultsText.textContent = `${playerWins} Rock destroys scissors. Welcome to the Jedi Council, Master.`;
-        }
+         resultsText.textContent = `${computerWins} Paper covers rock. I do not grant you the rank of Master.`;
+         document.getElementById('computerScore').textContent = computerScore++ + 1;
+    } else {
+        resultsText.textContent = `${playerWins} Rock destroys scissors. Welcome to the Jedi Council, Master.`;
+        document.getElementById('userScore').textContent = userScore++ + 1;
+    }
+}
 //PAPERBUTTON rock is 0, paper is 1, scissors is 2
 const userPaper = document.getElementById('paper');
 userPaper.addEventListener('click', gamePaper)
 function gamePaper() { 
     let computerSelection = getComputerChoice();
     if (computerSelection == selectionPool[0]) {
-        return resultsText.textContent = `${playerWins} Paper covers rock. Welcome to the Jedi Council, Master.`;
+            resultsText.textContent = `${playerWins} Paper covers rock. Welcome to the Jedi Council, Master.`;
+            document.getElementById('userScore').textContent = userScore++ + 1;
     } else if (computerSelection == selectionPool[1]) {
         return resultsText.textContent = noOneWins;
-    } else return resultsText.textContent = `${computerWins} Scissors slices paper. I do not grant you the rank of Master.`;
-        }
+    } else {
+        resultsText.textContent = `${computerWins} Scissors slices paper. I do not grant you the rank of Master.`;
+        document.getElementById('computerScore').textContent = computerScore++ + 1;
+    }
+}
 //SCISSORSBUTTON rock is 0, paper is 1, scissors is 2
 const userScissors = document.getElementById('scissors');
 userScissors.addEventListener('click', gameScissors)
 function gameScissors() { 
     let computerSelection = getComputerChoice();
     if (computerSelection == selectionPool[0]) {
-        return resultsText.textContent = `${computerWins} Rock destroys scissors. I do not grant you the rank of Master.`;
+        resultsText.textContent = `${computerWins} Rock destroys scissors. I do not grant you the rank of Master.`;
+        document.getElementById('computerScore').textContent = computerScore++ + 1;
     } else if (computerSelection == selectionPool[1]) {
-        return resultsText.textContent = `${playerWins} Scissors slices paper. Welcome to the Jedi Council, Master.`;
+         resultsText.textContent = `${playerWins} Scissors slices paper. Welcome to the Jedi Council, Master.`;
+         document.getElementById('userScore').textContent = userScore++ + 1;
+         
     } else return resultsText.textContent = noOneWins;
-        }
-
-
-
-
+}
